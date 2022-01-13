9f7486c43cc8df6dc8612db6d635eb91c15c24eb
YshuaStokes <51091683+YshuaStokes@users.noreply.github.com>
2022-01-13 12:45:10 -0600

    class challenges

---
+++ classChallengeSubmissionAssignment/classChallengeSubmissionAssignment.cpp
@@ -0,0 +1,52 @@
+// classChallengeSubmissionAssignment.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Shape
+{
+public:
+	Shape(string color) {
+		m_color = color;
+	}
+	void getArea() {
+		cout << "This will get the shape area, eventually when I code" << endl;
+	}
+
+
+public:
+	string m_color;
+};
+
+class Rect : public Shape
+{
+public:
+	int m_height;
+	int m_width;
+
+public:
+	Rect(string color, int height, int width) {
+		m_height = height;
+		m_width = width;
+	}
+};
+
+int main()
+{
+	
+
+	
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
